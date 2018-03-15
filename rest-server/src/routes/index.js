6d5d64a9df8b5e042cbcbb809c23b443385c3fb8
Jon <jcho4794@gmail.com>
2018-03-15 11:19:12 -0700

    rename matchRouter to matchmakingRouter

--- rest-server/src/routes/index.js
+++ rest-server/src/routes/index.js
@@ -2,14 +2,14 @@ import express from ('express');
 
 import authRouter from '../components/auth/authRouter';
 import userRouter from '../components/users/userRouter';
-import matchRouter from '../components/matches/matchRouter';
+import matchmakingRouter from '../components/matchmaking/matchmakingRouter';
 import successfulMatchRouter from '../components/successfulMatches/successfulMatchRouter';
 
 const router = express.Router;
 
 router.use('/auth', authRouter);
 router.use('/users', userRouter);
-router.use('/matches', matchRouter);
+router.use('/matchmaking', matchmakingRouter);
 router.use('/successfulMatches', successfulMatchRouter);
 router.use('/follows/', followRouter);
 
