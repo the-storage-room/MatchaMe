0b994e3fcd14ef00f3e563608b4054adcfaf9f26
Justin Dai <justingdai@gmail.com>
2018-03-16 12:00:01 -0700

    [Client] Working on Navbar

--- client/src/components/globals/Navbar/index.jsx
+++ client/src/components/globals/Navbar/index.jsx
@@ -4,15 +4,15 @@ import style from './Navbar.css';
 
 const Navbar = () => {
   return (
-    <div>
-      <div>Dash</div>
-      <div>Rate</div>
-      <div>MatchMake</div>
-      <div>Follows</div>
-      <div>My Match</div>
-      <div>Leaderboard</div>
-      <div>Avatar</div>
-    </div>
+    <h2 className={style.navContainer}>
+      <div className={style.navButton}>Dash</div>
+      <div className={style.navButton}>Rate</div>
+      <div className={style.navButton}>MatchMake</div>
+      <div className={style.navButton}>Follows</div>
+      <div className={style.navButton}>My Match</div>
+      <div className={style.navButton}>Leaderboard</div>
+      <div className={style.navButton}>Avatar</div>
+    </h2>
   );
 };
 
