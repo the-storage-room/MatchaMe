b3dfa2e12eb4320d597f4f3368594603e2ee59a8
Justin Dai <justingdai@gmail.com>
2018-03-16 11:29:25 -0700

    [Client] Dashboard and Navbar components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import LandingPage from './components/LandingPage/index.jsx';
 import Login from './components/Auth/Login.jsx';
 import Signup from './components/Auth/Signup.jsx';
+import Dashboard from './components/Dashboard/index.jsx';
 
 class App extends Component {
   constructor() {
@@ -17,6 +18,7 @@ class App extends Component {
         <Switch>
           <Route path="/login" component={Login} />
           <Route path="/signup" component={Signup} />
+          <Route path="/dashboard" component={Dashboard} />
           <Route path="/" component={LandingPage} />
         </Switch>
       </div>
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,13 +4,13 @@ const path = require('path');
 const Dotenv = require('dotenv-webpack');
 
 const SRC_DIR = path.join(__dirname, '/src');
-const PUBLIC_DIR = path.join(__dirname, '/public')
+const PUBLIC_DIR = path.join(__dirname, '/public');
 
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
-    filename: 'bundle.js', 
+    filename: 'bundle.js',
     path: PUBLIC_DIR
   },
   watch: true,
@@ -19,12 +19,26 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel-loader', 
+        loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015']
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[name]__[local]__[hash:base64:5]'
+            }
+          }
+        ]
       }
     ]
+<<<<<<< HEAD
   },
   plugins: [
     new Dotenv()
@@ -38,3 +52,7 @@ module.exports = {
 //     localIdentName: '[name]__[local]__[hash:base64:5]',
 //   },
 // }
+=======
+  }
+};
+>>>>>>> [Client] Dashboard and Navbar components
