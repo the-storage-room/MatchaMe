27f7a1086a4e57904f17438210619831c84ef2c6
Jon <jcho4794@gmail.com>
2018-03-16 11:54:57 -0700

    invoke Router() correctly, import followRouter

--- rest-server/src/routes/index.js
+++ rest-server/src/routes/index.js
@@ -4,8 +4,9 @@ import authRouter from '../components/auth/authRouter';
 import userRouter from '../components/users/userRouter';
 import matchmakingRouter from '../components/matchmaking/matchmakingRouter';
 import successfulMatchRouter from '../components/successfulMatches/successfulMatchRouter';
+import followRouter from '../components/follows/followRouter';
 
-const router = express.Router;
+const router = express.Router();
 
 router.use('/auth', authRouter);
 router.use('/users', userRouter);
