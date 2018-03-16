bf08dea7192ce5a6ae2306a5c315a01ec2605a5d
Justin Dai <justingdai@gmail.com>
2018-03-16 09:57:03 -0700

    [Client] Setting up React-Router and auth components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import LandingPage from './components/LandingPage/index.jsx';
+import Login from './components/Auth/Login.jsx';
+import Signup from './components/Auth/Signup.jsx';
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {};
   }
 
   render() {
-    return(
+    return (
       <div>
-        Hello, computer
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/" component={LandingPage} />
+        </Switch>
       </div>
     );
   }
