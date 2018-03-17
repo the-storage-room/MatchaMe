73ad0eeee27beb170400f2b01b0380fb6b95a0d6
Jon <jcho4794@gmail.com>
2018-03-17 13:33:55 -0700

    bcrypt

---
+++ rest-server/src/middleware/auth/bcrypt.js
@@ -0,0 +1,20 @@
+import {
+  compare,
+  genSalt,
+  hash
+} from 'bcrypt';
+
+const hashPassword = async (password) => {
+  const salt = await genSalt(JSON.parse(process.env.SALT_ROUNDS));
+  const hashed = await hash(password, salt);
+  return salt;
+};
+
+const comparePasswords = async (password, hash) => {
+  const result = compare(password, hash);
+  return result;
+};
+
+module.exports.hashPassword = hashPassword;
+module.exports.comparePasswords = comparePasswords;
+
