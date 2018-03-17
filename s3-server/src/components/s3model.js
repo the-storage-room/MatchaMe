c1d0a1d591ca9d6992e277c27d357764b0f1b75e
Jack Vaught <johnmvaught@gmail.com>
2018-03-17 12:48:41 -0700

    minor s3 code cleanup

--- s3-server/src/components/s3model.js
+++ s3-server/src/components/s3model.js
@@ -1,15 +1,16 @@
 import s3service from './s3service';
 
-const BUCKET = process.env.BUCKET;
+const { BUCKET } = process.env;
 
 module.exports = {
-  addPhoto: (input, callback) => {
-    s3service.addPhoto(BUCKET, input.files.file,(data) => {
+  addPhoto: (input, cb) => {
+    s3service.addPhoto(BUCKET, input.files.file, (data) => {
     // `https://s3-us-west-1.amazonaws.com/${BUCKET}/${input.files.file.name}`,
+    cb(data);
     })
   },
   deletePhoto: (input, callback) => {
-    s3service.deletePhoto(BUCKET, input.files.file.name,(data) => {
+    s3service.deletePhoto(BUCKET, input.files.file.name, (data) => {
       console.log('deleted')
     })
   },
--- s3-server/src/components/s3router.js
+++ s3-server/src/components/s3router.js
@@ -1,8 +1,8 @@
 import express from 'express';
+import s3controller from './s3controller';
 
 const router = express.Router();
 
-import s3controller from './s3controller';
 
 router.post('/s3', s3controller.addPhoto); 
 router.delete('/s3', s3controller.deletePhoto); 
