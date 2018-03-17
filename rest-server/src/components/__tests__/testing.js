0f23a9f92a026fda2be39d61c6eac791a39d3e4c
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-03-16 18:14:54 -0700

    [test] add tests for expected status code for /api/users

--- rest-server/src/components/__tests__/testing.js
+++ rest-server/src/components/__tests__/testing.js
@@ -1,9 +1,56 @@
 const request = require('supertest');
 const app = require('../../config/express/index');
 
-test('Testing jest connection', () => {
+test('Testing jest', () => {
   expect('true').toEqual('true')
 })
 
-request(app) 
-  .get('/')
+//user tests
+describe('fetchAllUsers', () => {
+  it('Should return status code of 200', () => {
+    return request(app)
+      .get('/api/users/fetchAllUsers')
+      .expect(200)
+  })
+})
+
+describe('fetchSingleUser', () => {
+  it('Should return status code of 200', () => {
+    return request(app)
+      .get('/api/users/fetchSingleUser')
+      .expect(200)
+  })
+})
+
+describe('fetchMultipleUsers', () => {
+  it('Should return status code of 200', () => {
+    return request(app)
+      .get('/api/users/fetchMultipleUsers/:userId')
+      .expect(200)
+  })
+})
+
+describe('updateUserRating', () => {
+  it('Should be able to update ratings', () => {
+    return Promise.all([
+      request(app)
+        .put('/api/users/updateUserRating')
+        .send({rating: '10'})
+        .expect(200),
+      request(app)
+        .get('/api/users/updateUserRating')
+        .then(res => {
+          expect(typeof res).toBe('string')
+        })
+    ])
+  })
+})
+
+describe('updateUserInfo', () => {
+  it('Should be able to update a info', () => {
+    return request(app)
+      .get('/api/users/updateUserInfo')
+      .expect(200)
+      
+  })
+})
