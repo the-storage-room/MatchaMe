7dee05f36994d4343328a20839c159ea567a4134
Jon <jcho4794@gmail.com>
2018-03-17 11:22:45 -0700

    global input component

---
+++ client/src/components/globals/Input/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import style from './Input.css';
+
+const Input = ({ type, name, onChange, placeholder }) => {
+  return (
+    <input 
+      type={type}
+      name={name}
+      onChange={onChange}
+      placeholder={placeholder}
+    />
+  );
+};
