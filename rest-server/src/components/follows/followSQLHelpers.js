991eef9755ea36d8773ef391cff810517bdbfab3
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-03-17 15:29:56 -0700

    [follow] single match queries filled

--- rest-server/src/components/follows/followSQLHelpers.js
+++ rest-server/src/components/follows/followSQLHelpers.js
@@ -1,34 +1,38 @@
 export const fetchStarredMatchesHelper = (userid) => {
   return `
-  SELECT username FROM follow
+  SELECT * FROM follow
   INNER JOIN MATCH ON follow.matchid = match.id
   INNER JOIN users ON follow.userid = users.id
-  OR match.user1_id = users.id OR match.user2_id = users.id
   WHERE userid = '${userid}'
   AND starred = '1'
   `;
 };
 
-export const fetchUnstarredMatchesHelper = () => {
+export const fetchUnstarredMatchesHelper = (userid) => {
   return `
-  SELECT username FROM follow
+  SELECT * FROM follow
   INNER JOIN MATCH ON follow.matchid = match.id
   INNER JOIN users ON follow.userid = users.id
-  OR match.user1_id = users.id OR match.user2_id = users.id
-  WHERE userid = '${userid}'
+  WHERE userid =${userid}
   AND starred = '0'
   `;
 };
 
-export const starSingleMatchHelper = () => {
+export const starSingleMatchHelper = (userid) => {
   return `
-
+  SELECT * FROM follow
+  INNER JOIN MATCH ON follow.matchid = match.id 
+  INNER JOIN users ON match.user1_id = users.id OR MATCH.user2_id = users.id
+  WHERE userid =${userid} 
   `;
 };
 
-export const unstarSingleMatchHelper = () => {
+export const unstarSingleMatchHelper = (userid) => {
   return `
-  
+  SELECT * FROM follow
+  INNER JOIN MATCH ON follow.matchid = match.id 
+  INNER JOIN users ON match.user1_id = users.id OR MATCH.user2_id = users.id
+  WHERE userid =${userid}
   `;
 };
 
