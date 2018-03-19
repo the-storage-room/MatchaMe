fea3f9a30faf1ab9130845b801a9b2fda66bf8ec
Jack Vaught <johnmvaught@gmail.com>
2018-03-19 15:00:12 -0700

    changing file structure again

--- client/src/components/AccountPage/index.jsx
+++ client/src/components/AccountPage/index.jsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import PhotoUpload from './../globals/PhotoUpload/index.jsx';
 
 import style from './AccountPage.css';
 import Navbar from '../globals/Navbar/index.jsx';
 
-const Account = props => {
-  return (
-    <div>
-      <Navbar />
-      <div className={style.upload}>
-        <PhotoUpload />
+class Account extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isFirstTimeUser: true,
+      currentPage: 1,
+    };
+  }
+
+  render () {
+    return (
+      <div>
+        {
+          this.state.isFirstTimeUser
+          ? <Navbar />
+          : null
+          }
+        <div className={style.upload}>
+          <PhotoUpload />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default withRouter(Account);
