4f844683af6f8aa0afe122b57de47360d3414efe
Jon Cho <jcho4794@gmail.com>
2018-03-19 18:16:20 -0700

    not needed

--- rest-server/src/components/users/userSQLHelper.js
+++ rest-server/src/components/users/userSQLHelper.js
@@ -31,57 +31,6 @@ export const updateUserInfoHelper = (setting, newInfo, username) => {
   `;
 };
 
-export const updateUserAgeHelper = (age, username) => {
-  return `
-    UPDATE users 
-    SET age='${age}'
-    WHERE username='${username}'
-    RETURNING age
-  `;
-};
-
-export const updateUserLocationHelper = (location, username) => {
-  return `
-    UPDATE users 
-    SET location='${location}'
-    WHERE username='${username}'
-    RETURNING location 
-  `;
-};
-
-export const updateUserGenderHelper = (gender, username) => {
-  return `
-    UPDATE users
-    SET gender='${gender}'
-    WHERE username='${username}'
-    RETURNING gender
-  `;
-};
-
-export const updateUserPreferenceHelper = (preference, username) => {
-  return `
-    UPDATE users 
-    SET preference='${preference}'
-    WHERE username='${username}'
-    RETURNING preference
-  `;
-};
-
-export const updateUserEmailHelper = (email, username) => {
-  return `
-    UPDATE users
-    SET email='${email}
-    WHERE username='${username}
-    RETURNING email
-  `;
-};
-
-export const updateFirstNameHelper = (firstName, username) => {
-  return `
-    UPDATE users
-  `;
-};
-
 // !! Will implement later ->
 
 // export const updateUsernameHelper = (newUsername, oldUsername) => {
