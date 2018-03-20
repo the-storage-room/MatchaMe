4a8f87bdcdcee0de37961c43b58a61bae8364c44
Jon <jcho4794@gmail.com>
2018-03-19 18:47:18 -0700

    sign up

--- rest-server/src/components/auth/authSQLHelpers.js
+++ rest-server/src/components/auth/authSQLHelpers.js
@@ -12,27 +12,11 @@ export const signupHelper = (body) => {
       username, 
       password, 
       email, 
-      lastname, 
-      firstname, 
-      age,
-      location, 
-      gender,
-      preference,
-      bio,
-      powerranking
     )
     VALUES (
       '${body.username}',
       '${body.password}',
       '${body.email}',
-      '${body.lastname}',
-      '${body.firstname}',
-       ${body.age},
-       ${body.location},
-      '${body.gender}',
-      '${body.preference}',
-      '${body.bio}',
-       ${body.powerranking}
     )
     RETURNING id, email, username
   `;
