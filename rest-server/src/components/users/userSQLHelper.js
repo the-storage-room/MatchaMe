6604b02a93aec8d5061acd2fa419ab09d1458f9b
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-03-21 09:56:51 -0700

    [power ranking] adding comments

--- rest-server/src/components/users/userSQLHelper.js
+++ rest-server/src/components/users/userSQLHelper.js
@@ -33,7 +33,7 @@ export const updateUserInfoHelper = (setting, newInfo, username) => {
 
 export const updateAcceptedUserRankingHelper = () => {
   return `
-  UPDATE USERs
+  UPDATE users
   SET powerranking = powerranking + '1'
   FROM outcomes
   INNER JOIN successfulmatch ON outcomes.matchid = successfulmatch.matchid
@@ -54,3 +54,33 @@ export const updateRejectedUserRankingHelper = () => {
   RETURNING *
   `
 }
+
+/*UPDATE users
+SET powerranking = powerranking - '1' 
+FROM outcomes
+INNER JOIN successfulmatch ON outcomes.matchid = successfulmatch.matchid
+WHERE successfulmatch.issuccessful = '0' 
+AND users.id = outcomes.userid
+RETURNING **/
+
+// -- issuccessful is true, increase for users who voted yes
+
+// UPDATE users
+// SET powerranking = powerranking + '1'
+// FROM yesUsers
+// INNER JOIN stagetwo ON stagetwo.matchid = yesUsers.matchid
+// WHERE stagetwo.issuccessful = '1'
+// AND users.id = yesUsers.id
+// --
+// UPDATE users
+// SET powerranking = powerranking + '1'
+// FROM outcomes
+// INNER JOIN stagtwo ON stagetwo.matchid = outcomes.matchid
+// WHERE successfulmatch.issuccessful = '1'
+// AND users.id = outcomes.userid
+// returning*
+
+// -- issuccessful is true, decrease for users who voted no
+// -- issuccessful is false, increase for users who voted no
+// -- issuccessful is false, decrease for users who voted yes
+
