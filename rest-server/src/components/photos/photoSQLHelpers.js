1f6d0d4701ceca9ffb000bc8ef6e533cb546a180
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-03-22 09:30:10 -0700

    [photo] include primary photo sql helper

--- rest-server/src/components/photos/photoSQLHelpers.js
+++ rest-server/src/components/photos/photoSQLHelpers.js
@@ -1,14 +1,20 @@
 export const fetchAllPhotosHelper = userId => {
   return `
+<<<<<<< HEAD
   SELECT id, url FROM Photo
   WHERE userid=${userId}
   ORDER BY primaryphoto DESC;
+=======
+
+>>>>>>> [photo] include primary photo sql helper
   `;
 };
 
-export const fetchPrimaryPhotoHelper = () => {
+export const fetchPrimaryPhotoHelper = (userId) => {
   return `
-  
+  SELECT photo.url FROM photo
+  INNER JOIN users ON users.id = photo.userid
+  WHERE primaryphoto=1 AND users.id='${userId}'
   `;
 };
 
