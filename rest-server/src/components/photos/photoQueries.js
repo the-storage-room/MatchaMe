8256cbe6a9f2a077d9c285b610bd16f54159bf86
Justin Dai <justingdai@gmail.com>
2018-03-22 13:52:16 -0700

    [Database] Photo controller and queries build

--- rest-server/src/components/photos/photoQueries.js
+++ rest-server/src/components/photos/photoQueries.js
@@ -12,16 +12,21 @@ export const fetchAllPhotosQuery = async userId => {
   try {
     const queryString = fetchAllPhotosHelper(userId);
     const { rows } = await db.query(queryString);
-    console.log('Success on fetchAllPhotosQuery')
+    console.log('Success on fetchAllPhotosQuery');
     return rows;
   } catch (err) {
-    console.log('Error on fetchAllPhotosQuery', err)
+    console.log('Error on fetchAllPhotosQuery', err);
   }
 };
 
-export const fetchPrimaryPhotoQuery = async body => {
+export const fetchPrimaryPhotoQuery = async userId => {
   try {
+    const queryString = fetchPrimaryPhotoHelper(userId);
+    const { rows } = await db.query(queryString);
+    console.log('Successfully used fetchPrimaryPhotoQuery');
+    return rows[0];
   } catch (err) {
+    console.log('Error with fetchPrimaryPhotoQuery', err);
   }
 };
 
@@ -32,11 +37,13 @@ export const addPhotoQuery = async body => {
 
 export const deletePhotoQuery = async body => {
   try {
-  } catch (err) {
-  }
+  } catch (err) {}
 };
 
 export const updatePrimaryPhotoQuery = async body => {
+  const queryString = updatePrimaryPhotoHelper(body);
+  const { rows } = await db.query(queryString);
+  return rows[0];
   try {
   } catch (err) {}
 };
--- rest-server/src/components/photos/photoSQLHelpers.js
+++ rest-server/src/components/photos/photoSQLHelpers.js
@@ -6,8 +6,11 @@ export const fetchAllPhotosHelper = userId => {
   `;
 };
 
-export const fetchPrimaryPhotoHelper = () => {
+export const fetchPrimaryPhotoHelper = userId => {
   return `
+  SELECT id, url FROM Photo
+  WHERE userid=${userId}
+  AND primaryphoto=1;
   `;
 };
 
@@ -22,8 +25,14 @@ export const deletePhotoHelper = () => {
   `;
 };
 
-export const updatePrimaryPhotoHelper = () => {
+export const updatePrimaryPhotoHelper = ({ userId, photoId }) => {
   return `
-  
+  UPDATE photo
+  SET primaryphoto=0
+  WHERE userid=${userId} AND primaryphoto=1;
+  UPDATE photo
+  SET primaryphoto=1
+  WHERE userid=${userId} AND id=${photoId}
+  RETURNING *;
   `;
 };
