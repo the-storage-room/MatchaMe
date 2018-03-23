411851a4a899ef7a8057c6e8a5668d2c1b0b0877
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-03-23 16:14:03 -0700

    resolve merege

--- client/src/components/FollowsPage/index.jsx
+++ client/src/components/FollowsPage/index.jsx
@@ -5,7 +5,6 @@ import style from './FollowsPage.css';
 import Navbar from '../globals/Navbar/index.jsx';
 // import StarredFollowsList from './StarredFollowsList'
 
-<<<<<<< HEAD
 const Follows = props => {
   return (
     <div>
@@ -16,29 +15,6 @@ const Follows = props => {
           {/* <StarredFollowsList /> */}
         </div>
         <div className={style.resultsContainer}>Render All Matches Here!</div>
-=======
-// const Follows = props => {
-//   return (
-//     <div>
-//       <Navbar />
-//       <div className={style.followsContainer}>
-//         <div className={style.starredContainer}>
-//           Render Starred matches Here
-//           <StarredFollowsList />
-//         </div>
-//         <div className={style.resultsContainer}>Render All Matches Here!</div>
-//       </div>
-//     </div>
-//   );
-// };
-
-class Follow extends React.Component => {
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <div className={style.followContainer}
->>>>>>> 1176fa07ad51471a090922b6246e8726139f8b98
       </div>
     )
   }
