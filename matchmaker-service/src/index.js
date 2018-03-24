0644685d1276e24a83883635c6bfc3770e082e0c
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-03-24 16:17:35 -0700

    [mm service] creating file structure

--- matchmaker-service/src/index.js
+++ matchmaker-service/src/index.js
@@ -1,5 +1,8 @@
-import App from './config';
+import App from './config/express/index';
+import db from './config/database/index';
 
 const { PORT } = process.env;
 
 App.listen(PORT, () => console.log('Matchmaker server listening on port ', PORT));
+
+db.connect();
