9eff5795ef2cf75956a4bddbff0245a7509b8da1
Jack Vaught <johnmvaught@gmail.com>
2018-03-25 00:05:33 -0700

    [redux] adds followsItem and Container

---
+++ client/src/components/FollowsPage/FollowsContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import style from './FollowsPage.css';
+import FollowsItem from './FollowsItem.jsx';
+
+const FollowsContainer = ({ type, data, handleStar }) => {
+  return (
+    <div>
+      hi
+    </div>
+  )
+};
+
+export default FollowsContainer;
