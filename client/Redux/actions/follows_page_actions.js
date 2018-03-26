ff17f130726d34dc87a980fab6ea24b4d8d944d9
Jack Vaught <johnmvaught@gmail.com>
2018-03-26 14:57:54 -0700

    follows page works with stars

--- client/Redux/actions/follows_page_actions.js
+++ client/Redux/actions/follows_page_actions.js
@@ -6,8 +6,13 @@ export default {
   starFollow(matchId, index) {
     return async (dispatch, getState) => {
       const { id } = getState().accountData;
-      const { starred, allOthers } = getState().follows;
-      
+      const { follows } = getState();
+      const newFollows = JSON.parse(JSON.stringify(follows))
+      const { starred, allOthers } = newFollows;
+      allOthers[index].starred = 1;
+      const movingMatch = allOthers.splice(index, 1)
+      starred.push(movingMatch[0])
+      starred.sort((a, b) => a.id - b.id)
       try {
       await axios
         .put(`${REST_SERVER_URL}/api/outcomes/starSingleMatch/${id}/${matchId}`)
@@ -23,6 +28,15 @@ export default {
   unstarFollow(matchId, index) {
     return async (dispatch, getState) => {
       const { id } = getState().accountData;
+      const { follows }  = getState();
+      const newFollows = JSON.parse(JSON.stringify(follows))
+      const { starred, allOthers } = newFollows;
+      starred[index].starred = 0;
+      const movingMatch = starred.splice(index, 1)
+      allOthers.push(movingMatch[0])
+      allOthers.sort((a, b) => a.id - b.id)
+      newFollows.starred = starred;
+      newFollows.allOthers = allOthers;
       try {
       await axios
         .put(`${REST_SERVER_URL}/api/outcomes/unstarSingleMatch/${id}/${matchId}`)
