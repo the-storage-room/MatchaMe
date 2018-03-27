372763168d2a4cf362f2e006ccad4acdc8d482a6
Justin Dai <justingdai@gmail.com>
2018-03-27 10:00:57 -0700

    [Redis] Completed fetch request from redis server

--- redis-server/src/config/redis/index.js
+++ redis-server/src/config/redis/index.js
@@ -1,37 +1,36 @@
 import redis from 'redis';
 import axios from 'axios';
+import Promise from 'bluebird';
 
-export const client = redis.createClient();
+const client = Promise.promisifyAll(redis.createClient());
 
 client.on('connect', () => {
   console.log('Connected to Redis Server');
-  setInterval(getAllRanks, 1000 * 60 * 55);
+  setInterval(getAllRanks, 1000 * 60);
+  getAllRanks();
 });
 
 const { REST_SERVER_URL } = process.env;
 
-const getAllRanks = () => {
-  client.ttl('leaderboard', async (err, timeLeft) => {
-    if (err) console.log(err);
-    try {
-      if (timeLeft < 0) {
-        const time = client.ttl('leaderboard');
-        console.log(time);
-        const { data } = await axios.get(
-          `${REST_SERVER_URL}/api/users/fetchAllUsers/`
-        );
-        for (let user of data) {
-          client.lpush('leaderboard', JSON.stringify(user), (err, rank) => {
-            if (err) console.log(err);
-            client.set(`${user.id}`, rank);
-          });
-        }
-        client.expire('leaderboard', 60);
-      } else {
-        console.log('Already done!');
+const getAllRanks = async () => {
+  try {
+    let timeLeft = await client.ttlAsync('leaderboard');
+    if (timeLeft < 15) {
+      console.log(timeLeft);
+      const { data } = await axios.get(
+        `${REST_SERVER_URL}/api/users/fetchAllUsers/`
+      );
+      for (let user of data) {
+        let rank = await client.rpushAsync('leaderboard', JSON.stringify(user));
+        client.setex(`${user.id}`, 60, rank);
       }
-    } catch (err) {
-      console.log('Error with axios request', err);
+      client.expire('leaderboard', 60);
+    } else {
+      console.log('Already done!');
     }
-  });
+  } catch (err) {
+    console.log('Error with retrieving users', err);
+  }
 };
+
+export default client;
--- redis-server/src/index.js
+++ redis-server/src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import client from './config/redis/';
-// import router from './components/redisRouter';
+import router from './components/redisRouter';
 
 const app = express();
 
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use('/redis', router);
+app.use('/redis', router);
 
 const { PORT } = process.env;
 
