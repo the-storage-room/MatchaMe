8a3ed09d93cc8100f8a8b78999eec4f73f167fd1
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-03-27 19:42:14 -0700

    delete func

--- matchmaker-service/src/helpers/averageAttractiveness/averageAttractivenessSQLHelper.js
+++
@@ -1,6 +0,0 @@
-export const findAvgAttractivenessHelper = (usersRating) => {
-  return `
-  SELECT * FROM users
-  WHERE averageattractiveness = ${usersRating}+3 OR averageattractiveness = ${usersRating}-3
-  `;
-}
