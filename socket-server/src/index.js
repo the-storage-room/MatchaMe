7834830e69899b60f9d5de06d28ef44d732f55ee
Jack Vaught <johnmvaught@gmail.com>
2018-03-28 10:52:10 -0700

    starting socket chat

--- socket-server/src/index.js
+++ socket-server/src/index.js
@@ -1,14 +1,20 @@
 import http from 'http';
 import SocketIo from 'socket.io';
 
-// const Rooms = require('./rooms');
-// const clientEvents = require('./clientEvents');
+import clientEvents from './socketEvents/clientEvents';
 
 const server = http.createServer();
 const io = SocketIo(server);
 
 io.on('connection', (client) => {
   console.log('client connected');
+  const { user1id, user2id } = client.handshake.query;
+  const room = [user1id, user2id].sort();
+  client.join(room);
+
+  clientEvents.forEach((handler, event) => {
+    client.on(event, handler.bind(null, { io, client, room }));
+  });
 });
 
 const { PORT } = process.env;
