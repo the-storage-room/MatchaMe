b89ef3ff68bdb7a1d49bcd9e3d39f6ba9a9b8931
Jack Vaught <johnmvaught@gmail.com>
2018-03-27 21:25:06 -0700

    [currentMatch] warning modal created

--- client/src/components/MyMatchPage/WarningBox.jsx
+++ client/src/components/MyMatchPage/WarningBox.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import style from './MyMatchPage.css';
 import Profile from '../globals/Profile/index.jsx';
 import Button from '../globals/Button/index.jsx';
 
-const WarningBox = () => {
-  return (
-    <div className={style.noMatch}>
-      WarningBox!
-    </div>
-  );
+class WarningBox extends Component {
+  render() {
+    if(!this.props.show) {
+      return null;
+    }
+
+    return (
+      <div className={style.warningBackdrop}>
+        <div className={style.warningBox}>
+          <div>
+            Are you sure? You will NEVER see this person again!
+            <Button 
+              onClick={() => this.props.endMatch()}
+              text={"Yes. END MATCH"}
+              />
+            <Button 
+              onClick={this.props.onClose}
+              text={"NO. Go Back!"}
+              />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default WarningBox;
