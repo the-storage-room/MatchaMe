79d1b71e8bc8a78c8d431605d4abd3832eb01cc2
Jack Vaught <johnmvaught@gmail.com>
2018-03-28 23:35:03 -0700

    [Frontend] Leaderboard component now classical

--- client/src/components/LeaderboardPage/index.jsx
+++ client/src/components/LeaderboardPage/index.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import style from './LeaderboardPage.css';
 import Navbar from '../globals/Navbar/index.jsx';
 
-const Leaderboard = props => {
-  return (
-    <div>
-      <Navbar />
-      <div className={style.leaderboardContainer}>
-        <div />
-      </div>
-    </div>
-  );
-};
+class Leaderboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
 
-export default withRouter(Leaderboard);
+    };
+  }
+
+  render() {
+    console.log(this.props)
+    return (
+      <div>
+        <Navbar />
+        <div className={style.leaderboardContainer}>
+          <div />
+        </div>
+      </div>
+    );
+  }
+}
+
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+
+  }, dispatch);
+}
+
+const mapStateToProps = ({ Leaderboard }) => {
+  return {
+    Leaderboard,
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Leaderboard);
