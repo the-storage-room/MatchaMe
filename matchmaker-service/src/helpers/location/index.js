1cff9a2d70fcd3af7c0f9cf2c75d8e79b35f01a2
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-03-28 19:57:40 -0700

    able to add to local file

--- matchmaker-service/src/helpers/location/index.js
+++ matchmaker-service/src/helpers/location/index.js
@@ -25,48 +25,6 @@ const distanceBetweenCoordinates = (userOneLon, userOneLat, userTwoLon, userTwoL
 };
 
 //takes in two zipcodes, if zipcode doesn't exist in local file, it will ping the API
-// const findCoordinatesAndCalculateDistance = async (zipcode1, zipcode2) => {
-// 	try {
-// 		let user1Lat;
-// 		let user1Lng;
-// 		let user2Lat;
-// 		let user2Lng;
-// 		const { API_KEY } = process.env;
-// 		const data = await fs.readFileAsync(__dirname + '/zipcodesAndGeocodes.json', 'utf-8');
-// 		let parsedData = JSON.parse(data);
-// 		if (!parsedData[zipcode1]) {
-// 			const userOneObj = await axios.get(
-// 				`https://maps.googleapis.com/maps/api/geocode/json?address=${zipcode1}&key=${API_KEY}`
-// 			);
-// 			const { lat, lng } = userOneObj.data.results[0].geometry.location;
-// 			const objToSendToLocal = JSON.stringify({ zipcode1: { lat: lat, lng: lng } });
-// 			console.log(objToSendToLocal)
-// 			user1Lat = lat;
-// 			user1Lng = lng;
-// 			await fs.writeFileAsync('zipcodesAndGeocodes.json', 'hi', 'utf-8')
-// 		} else if (!parsedData[zipcode2]) {
-// 			const userTwoObj = await axios.get(
-// 				`https://maps.googleapis.com/maps/api/geocode/json?address=${zipcode2}&key=${API_KEY}`
-// 			);
-// 			const { lat, lng } = userTwoObj.data.results[0].geometry.location;
-// 			user2Lat = lat;
-// 			user2Lng = lng;
-// 		} else {
-// 			let geocodeObj1 = parsedData[zipcode1];
-// 			let geocodeObj2 = parsedData[zipcode2];
-// 			user1Lat = geocodeObj1.lat;
-// 			user1Lng = geocodeObj1.lng;
-// 			user2Lat = geocodeObj2.lat;
-// 			user2Lng = geocodeObj2.lng;
-// 		}
-// 		let distanceExact = distanceBetweenCoordinates(user1Lng, user1Lat, user2Lng, user2Lat);
-// 		let finalDistance = Math.floor(distanceExact);
-// 		console.log('success on calculateDistance', finalDistance);
-// 		return finalDistance;
-// 	} catch (err) {
-// 		console.log('error on calculateDistance', err);
-// 	}
-// };
 const findCoordinatesAndCalculateDistance = async (zipcode1, zipcode2) => {
 	try {
 		let user1Lat;
@@ -76,12 +34,38 @@ const findCoordinatesAndCalculateDistance = async (zipcode1, zipcode2) => {
 		const { API_KEY } = process.env;
 		const data = await fs.readFileAsync(__dirname + '/zipcodesAndGeocodes.json', 'utf-8');
 		let parsedData = JSON.parse(data);
-		let geocodeObj1 = parsedData[zipcode1];
-		let geocodeObj2 = parsedData[zipcode2];
-		user1Lat = geocodeObj1.lat;
-		user1Lng = geocodeObj1.lng;
-		user2Lat = geocodeObj2.lat;
-		user2Lng = geocodeObj2.lng;
+
+		if (!parsedData[zipcode1]) {
+			const userOneObj = await axios.get(
+				`https://maps.googleapis.com/maps/api/geocode/json?address=${zipcode1}&key=${API_KEY}`
+			);
+			const { lat, lng } = userOneObj.data.results[0].geometry.location;
+			user1Lat = lat;
+			user1Lng = lng;
+			parsedData[`${zipcode1}`] = {lat: lat, lng:lng}
+			let newData = JSON.stringify(parsedData)
+			await fs.writeFileAsync(__dirname + '/zipcodesAndGeocodes.json', newData, 'utf-8')
+		} else if (!parsedData[zipcode2]) {
+			const userTwoObj = await axios.get(
+				`https://maps.googleapis.com/maps/api/geocode/json?address=${zipcode2}&key=${API_KEY}`
+			);
+			const { lat, lng } = userOneObj.data.results[0].geometry.location;
+			user2Lat = lat;
+			user2Lng = lng;
+			parsedData[`${zipcode1}`] = {lat: lat, lng:lng}
+			let newData = JSON.stringify(parsedData)
+			await fs.writeFileAsync(__dirname + '/zipcodesAndGeocodes.json', newData, 'utf-8')
+		}
+		if (parsedData[zipcode1]) {
+			let geocodeObj1 = parsedData[zipcode1];
+			user1Lat = geocodeObj1.lat;
+			user1Lng = geocodeObj1.lng;
+		} 
+		if (parsedData[zipcode2]) {
+			let geocodeObj2 = parsedData[zipcode2];
+			user2Lat = geocodeObj2.lat;
+			user2Lng = geocodeObj2.lng;
+		}
 		let distanceExact = distanceBetweenCoordinates(user1Lng, user1Lat, user2Lng, user2Lat);
 		let finalDistance = Math.floor(distanceExact);
 		console.log('success on calculateDistance', finalDistance);
@@ -91,4 +75,3 @@ const findCoordinatesAndCalculateDistance = async (zipcode1, zipcode2) => {
 	}
 };
 
-findCoordinatesAndCalculateDistance(92841, 90045);
