6ff65261ccf1d22150c54503ccb1dad09ab3450f
Jack Vaught <johnmvaught@gmail.com>
2018-03-31 00:42:08 -0700

    [frontend] Navbar 1.1

--- client/Redux/reducers/accountDataReducer.js
+++ client/Redux/reducers/accountDataReducer.js
@@ -1,5 +1,5 @@
 
-export default (state = {id: 2, username: 'jack'}, action) => {
+export default (state = {id: 3, username: 'jack'}, action) => {
   switch (action.type) {
     case 'USER_ACCOUNT_DATA_RECIEVED':
       return (state = action.payload);
