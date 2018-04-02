b7474a5639c961524f32a75383ffe5395de46ffb
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-04-02 10:07:03 -0700

    dropdown menu

--- client/src/components/globals/Avatar/index.jsx
+++ client/src/components/globals/Avatar/index.jsx
@@ -4,28 +4,26 @@ import { connect } from 'react-redux';
 import style from './Avatar.css';
 
 class Avatar extends React.Component {
-  constructor() {
-    super();
-    this.state = {
+	constructor() {
+		super();
+		this.state = {};
+	}
 
-    };
-  }
-
-  render() {
-    return (
-      <img 
-        className={style.avatar}
-        src={this.props.photo}
-        onClick={() => this.props.history.push('/account/bio')}
-      />
-    );
-  }
+	render() {
+		return (
+			<img
+				className={style.avatar}
+				src={this.props.photo}
+				// onClick={() => this.props.history.push('/account/bio')}
+			/>
+		);
+	}
 }
 
-const mapStateToProps = (state) => {
-  return {
-    photo: state.userPhotos[0].url
-  }
-}
+const mapStateToProps = state => {
+	return {
+		photo: state.userPhotos[0].url,
+	};
+};
 
 export default connect(mapStateToProps)(Avatar);
