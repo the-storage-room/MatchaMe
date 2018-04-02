05fd0041694a4230202e04bfd40e976db09191e4
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-04-02 15:52:26 -0700

    [UI] photo centered

--- client/src/components/Dashboard/LeaderboardItem.jsx
+++ client/src/components/Dashboard/LeaderboardItem.jsx
@@ -4,18 +4,13 @@ import style from './Dashboard.css';
 const LeaderboardItem = ({ powerranking, primaryPhoto, username, index }) => {
 	return (
 		<div className={style.boardwrapper}>
-			<div className={style.leaderboardItems}>
-				<div className={style.col1}>
-					<div className={style.rank}>{index + 1}</div>
-					<img className={style.avatar} src={primaryPhoto.url} />
-					<div className={style.username}>{username}</div>
-				</div>
-				<div className={style.col2}>
-					<div className={style.pRC}>
-						<div className={style.powerrankingTitle}>TOTAL POINTS</div>
-						<div className={style.powerranking}>{powerranking}</div>
-					</div>
-				</div>
+			<div className={style.rank}>{index + 1}</div>
+			<img className={style.avatar} src={primaryPhoto.url} />
+			<div className={style.username}>{username}</div>
+
+			<div className={style.pRC}>
+				<div className={style.powerrankingTitle}>TOTAL POINTS: </div>
+				<div className={style.powerranking}>{powerranking}</div>
 			</div>
 		</div>
 	);
