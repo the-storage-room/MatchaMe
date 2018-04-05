678f0e03f38a3b40322f51434a02796c8c8f759a
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-04-04 17:56:12 -0700

    [security] change file names

--- client/Redux/reducers/protectedSignUpReducer.js
+++
@@ -1,8 +0,0 @@
-export default (state = {}, action) => {
-  switch (action.type) {
-    case 'USER_SIGNUP_STATUS_RECEIVED':
-      return (state = action.payload);
-    default:
-      return state;
-  }
-};
