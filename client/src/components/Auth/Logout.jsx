ae03d5e87da871ce277f9417fd58dcd5d5022f89
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-04-04 19:27:10 -0700

    [auth] redirect to landing after logout

--- client/src/components/Auth/Logout.jsx
+++ client/src/components/Auth/Logout.jsx
@@ -1,18 +1,37 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
 const { REST_SERVER_URL } = process.env;
 
-const Logout = async (props) => {
-  console.log('props from logout', props)
-  try {
-    const data = await axios.get(`${REST_SERVER_URL}/api/auth/logout`)
-    localStorage.clear();
-    props.history.push('/login')
-  } catch (err) {
-    console.log('error on logout', err)
+// const Logout = async (props) => {
+//   try {
+//     await axios.get(`${REST_SERVER_URL}/api/auth/logout`)
+//     localStorage.clear();
+//     props.history.push('/')
+//     return null;
+//   } catch (err) {
+//     console.log('error on logout', err)
+//   }
+// }
+
+class Logout extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  logoutUser = async () => {
+    try {
+      await axios.get(`${REST_SERVER_URL}/api/auth/logout`)
+      localStorage.clear();
+    } catch (err) {
+      console.log('Error on logout', err)
+    }
+  }
+
+  render() {
+    this.props.history.push('/');
+    return null;
   }
 }
-
 
 export default Logout;
