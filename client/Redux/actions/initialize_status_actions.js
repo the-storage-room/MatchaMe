7a9f921618d82b2116c886c9f8bcdaf61fb4351e
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-04-05 19:57:11 -0700

    commit before rebase

---
+++ client/Redux/actions/initialize_status_actions.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const { REST_SERVER_URL } = process.env;
+
+export default {
+  updateInitializeState() {
+    return async (dispatch, getState) => {
+      let { initializeState } = getState();
+      console.log('initial state from action', initializeState)
+      try {
+        dispatch({
+          type: 'INITIALIZE_STATUS_TRUE',
+        })
+      } catch (err) {
+        console.log('error on update initialize state action', err)
+      }
+    }
+  }
+}
