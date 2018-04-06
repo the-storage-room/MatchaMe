001d9946668d6b9760683b07c44b4c9ea19b36f3
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-04-05 19:59:24 -0700

    restore

--- client/src/components/globals/Protected/index.js
+++ client/src/components/globals/Protected/index.js
@@ -1,61 +1,53 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import store from '../../../../Redux'
+// import store from '../../../../Redux'
 import jwtDecode from 'jwt-decode';
-import actions from '../../../../Redux/actions/signup_status_actions';
+import action from '../../../../Redux/actions/initialize_status_actions';
+import initialize from '../../../../Redux/actions/initialize_actions';
 
 class Protected extends Component {
   constructor() {
     super();
-    this.state = {
-      pathname: '',
+  }
+
+  componentDidMount = async () => {
+    try {
+      const { signupStatus, history, location, component, initializeState, initializeStateAction, initialize } = this.props;
+      const path = location.pathname.slice(1, 11);
+      if (!localStorage.token) {
+        history.push('/login')
+      }
+      const { exp } = jwtDecode(localStorage.token);
+      const roundedExp = Math.floor(Date.now() / 1000)
+      if (exp < roundedExp) {
+        history.push('/login')
+      }
+      if (initializeState === false) {
+        await initialize(history, location.pathname)
+        initializeStateAction()
+      } else if (signupStatus === false && path !== 'onboarding') {
+        history.push('/onboarding/bio')
+      }
+    } catch (err) {
+      console.log('error in protected', err)
     }
   }
 
-  // componentDidMount() {
-  //   try {
-  //     const { NODE_ENV } = process.env;
-  //     const { signupStatus, history, location, component, initializeState } = this.props;
-  //     if (NODE_ENV = 'PRODUCTION') {
-  //       if (!localStorage.token) {
-  //         history.push('/login')
-  //       } else {
-  //         const { exp } = jwtDecode(localStorage.token);
-  //         const path = location.pathname.slice(1, 11);
-  //         if (exp > Math.floor(Date.now() / 1000) && signupStatus === false && path !== 'onboarding') {
-  //           history.push('/onboarding/bio')
-  //         } else if (exp > Math.floor(Date.now() / 1000) && signupStatus === true) {
-  //           console.log('initialize', initializeState)
-  //           console.log('store', store.getState())
-  //           if (initializeState === false && component.name !== 'HomePage') {
-  //             console.log('initializeState', initializeState)
-  //             initializeState = true;
-  //             history.push('/initialize')
-  //           }
-  //         } else if (exp < Math.floor(Date.now() / 1000)) {
-  //           history.push('/login')
-  //         }
-  //   } catch (err) {
-  //     console.log('error in protected', err)
-  //   }
-  // }
-
   render() {
     const { component: Component } = this.props;
-    console.log('this.props from render', this.props)
     return <Component {...this.props} />;
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return bindActionCreators({
-    signupstatus: actions.fetchUserSignupStatus
+    initializeStateAction: action.updateInitializeState,
+    initialize: initialize.initialize
   }, dispatch)
 }
 
 const mapStateToProps = state => {
-  console.log('staaaaaate', state)
   return {
     signupStatus: state.signupStatus,
     initializeState: state.initializeState,
